03a0fed8167b16799a3f500d85d1b5181c87b87c
Arthur Blanco <arthurpblanco@gmail.com>
2021-08-11 17:08:29 -0300

    agenda_contatos

---
+++ lib/provider/contacts.dart
@@ -0,0 +1,52 @@
+import 'package:agenda_contatos/models/contact.dart';
+
+List <Contact>contacts = [
+  Contact(
+  name: "Nicole Santos", 
+  photo: "assets/images/Nicole.jpg", 
+  phone: "+55 51 91111-1111", 
+  email: "nicolefadasensata@gmail.com"
+  ),
+  Contact(
+  name: "Romildo Bonzamm", 
+  photo: "assets/images/Thomas.jpg", 
+  phone: "+55 51 95215-7536", 
+  email: "romildogremio@gmail.com"
+  ),
+  Contact(
+  name: "Sandra Lupinsk", 
+  photo: "assets/images/Camila.jpg", 
+  phone: "+55 51 97842-7826", 
+  email: "sandrinha020@gmail.com"
+  ),
+  Contact(
+  name: "vitor ludwig", 
+  photo: "assets/images/Paulo.jpg", 
+  phone: "+55 51 97623-4582", 
+  email: "vitinhogameplays@gmail.com"
+  ),
+  Contact(
+  name: "Luciano Huck", 
+  photo: "assets/images/Lucas.jpg", 
+  phone: "+55 51 96667-5554", 
+  email: "latavelha@gmail.com"
+  ),
+  Contact(
+  name: "Carol Portaluppi", 
+  photo: "assets/images/Juliana.jpg", 
+  phone: "+55 51 97846-7512", 
+  email: "carolportagremio@gmail.com"
+  ),
+  Contact(
+  name: "Regina Casé", 
+  photo: "assets/images/Claudia.jpg", 
+  phone: "+55 51 90045-4513", 
+  email: "exxxxquentaa@gmail.com"
+  ),
+  Contact(
+  name: "Cris Paiva", 
+  photo: "assets/images/Marina.jpg", 
+  phone: "+55 51 91562-7830", 
+  email: "criscrispaiva@gmail.com"
+  ),
+];
---
+++ lib/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'style.dart';
+
+var apptheme = ThemeData(
+  floatingActionButtonTheme: 
+        FloatingActionButtonThemeData(
+          backgroundColor: redTheme,
+        ),
+        primarySwatch: blueTheme,
+        dividerTheme: DividerThemeData(
+          color: grayDivider,
+          thickness: 1
+        )
+      
+      );
